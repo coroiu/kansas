150e9e22b5a71a00e4b23410549e6c37a34c05c4
Andreas Coroiu <andreas.coroiu@gmail.com>
2018-05-05 18:04:20 +0200

    Add test endpoint.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Welcome to Kansas!")
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("got /test request")
+		w.Write([]byte("hello world"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
