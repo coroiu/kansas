b138664de38b6e5f470a8fbde6bd83d8b2d17b69
Andreas Coroiu <andreas.coroiu@gmail.com>
2018-05-06 17:35:54 +0200

    Add hostname flag and listen to it.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import "fmt"
+import "flag"
 import "net/http"
 
 func main() {
 	fmt.Println("Welcome to Kansas!")
 
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+	host := flag.String("host", "localhost", "hostname to use for proxying")
+	flag.Parse()
+
+	fmt.Println("Using '" + *host + "' as hostname")
+
+	http.HandleFunc(*host+"/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("got /test request")
 		w.Write([]byte("hello world"))
 	})
